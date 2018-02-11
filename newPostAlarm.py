01b970225bc600315dcb9c246a50092968f6bf1f
ojlee <dldhwn56@gmail.com>
2018-02-11 18:05:41 +0900

    rm caffename2

--- newPostAlarm.py
+++ newPostAlarm.py
@@ -5,8 +5,8 @@ import os
 
 import telegram
 
-bot = telegram.Bot(token='540032183:AAEHc2AJ3oXfKY8Hi8a56XSRQJIYfvPotmI')
-url = 'https://goo.gl/DZ46oi'
+bot = telegram.Bot(token='')
+url = 'https://goo.gl/6iRR5a'
 
 
 chat_id = bot.getUpdates()[-1].message.chat.id
@@ -22,25 +22,24 @@ soup = BeautifulSoup(html, 'html.parser')
 
 titles = soup.select('#elThumbnailResultArea > li > dl > dt > a')
 postsText = soup.select('#elThumbnailResultArea > li:nth-of-type(1) > dl > dd.sh_cafe_passage')
-cafeNames = soup.select('#elThumbnailResultArea > li:nth-of-type(1) > dl > dd.txt_block > span > a:nth-of-type(1)')
 
-if cafeNames[0].text.strip() == '중고나라':
-    latest_title = titles[0].text.strip() # 첫번째 글 자리
-    latest_text = postsText[0].text.strip()
-    second_title = titles[1].text.strip() # 두번째 글 자리
-    third_title = titles[2].text.strip() # 세번째 글 자리
 
-    with open(os.path.join(BASE_DIR, 'latest.txt'), 'r+') as f_read:
-        before = f_read.readline()
-        f_read.close()
-        if before != latest_title:
-            # 같은 경우는 에러 없이 넘기고, 다른 경우에만
-            bot.sendMessage(chat_id=chat_id, text='새 글!\n\n제목 - '+ latest_title +
-                            '\n내용 - ' + latest_text +
-                            '\n\n둘째 - ' + second_title + '\n셋째 - ' + third_title
-                            +'\n\n' + '\n목록\n'+url)
-            with open(os.path.join(BASE_DIR, 'latest.txt'), 'w+') as f_write:
-                f_write.write(latest_title)
-                f_write.close()
+latest_title = titles[0].text.strip() # 첫번째 글 자리
+latest_text = postsText[0].text.strip()
+second_title = titles[1].text.strip() # 두번째 글 자리
+third_title = titles[2].text.strip() # 세번째 글 자리
+
+with open(os.path.join(BASE_DIR, 'latest.txt'), 'r+') as f_read:
+    before = f_read.readline()
+    f_read.close()
+    if before != latest_title:
+        # 같은 경우는 에러 없이 넘기고, 다른 경우에만
+        bot.sendMessage(chat_id=chat_id, text='새 글!\n\n제목 - '+ latest_title +
+                        '\n내용 - ' + latest_text +
+                        '\n\n둘째 - ' + second_title + '\n셋째 - ' + third_title
+                        +'\n\n' + '\n목록\n'+url)
+        with open(os.path.join(BASE_DIR, 'latest.txt'), 'w+') as f_write:
+            f_write.write(latest_title)
+            f_write.close()
 
 
